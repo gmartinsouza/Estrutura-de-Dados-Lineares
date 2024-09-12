491a2c9f37aa9e97710dc34e6f581bd9ec94debd
Gabriel Martins <163541683+gmartinsouza@users.noreply.github.com>
2024-09-12 16:05:50 -0300

    Create stack.h

---
+++ stacks/stack.h
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+
+typedef struct stack Stack;
+
+
+Stack * Stack_Create();
+void Stack_Destroy(Stack * stack);
+bool Stack_IsEmpty(Stack * stack);
+void Stack_Push(Stack * stack, int value);
+int Stack_Pop(Stack * stack);
+int Stack_Peek(Stack * stack);
