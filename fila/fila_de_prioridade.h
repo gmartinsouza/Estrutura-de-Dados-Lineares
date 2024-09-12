ced177ef8ae5417bc3da34ab455a353fdb85625f
Gabriel Martins <163541683+gmartinsouza@users.noreply.github.com>
2024-09-12 16:09:31 -0300

    Delete fila directory

--- fila/fila_de_prioridade.h
+++
@@ -1,8 +0,0 @@
-#include <stdbool.h>
-typedef struct priorityQueue PriorityQueue;
-
-PriorityQueue * PriorityQueue_Create();
-void PriorityQueue_Destroy(PriorityQueue * queue);
-bool PriorityQueue_IsEmpty(PriorityQueue * queue);
-void PriorityQueue_Enqueue(PriorityQueue * queue, int value);
-int PriorityQueue_Dequeue(PriorityQueue * queue);
